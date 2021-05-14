407991c2aa5e721fbe2aa8baa6f923d6e38b66b7
Markéta Vošická <vosickamarketa@gmail.com>
2021-05-14 23:16:21 +0200

    vypracován úkol dle zadání

---
+++ src/main/java/cz/czechitas/java2webapps/ukol6/service/CitatyService.java
@@ -0,0 +1,24 @@
+package cz.czechitas.java2webapps.ukol6.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CitatyService {
+    private final NahodneCisloService nahodneCisloService;
+    private final CitatyRepository citatyRepository;
+
+    @Autowired
+    public CitatyService(NahodneCisloService nahodneCisloService, CitatyRepository citatyRepository) {
+        this.nahodneCisloService = nahodneCisloService;
+        this.citatyRepository = citatyRepository;
+    }
+
+    public String nahodnyCitat() {
+        return citatyRepository.citat(nahodneCisloService.dejNahodneCislo(citatyRepository.pocet()));
+    }
+
+    public String konkretniCitat(int cislo) {
+        return citatyRepository.citat(cislo - 1);
+    }
+}
